1dc826715a36c840dc54d4d9e8f6137695344f72
Thomas Alexander Gerds <tag@biostat.ku.dk>
2015-10-05 09:39:20 +0200

    added print functions

---
+++ R/print.calibrationPlot.R
@@ -0,0 +1,70 @@
+### print.calibrationPlot.R --- 
+#----------------------------------------------------------------------
+## author: Thomas Alexander Gerds
+## created: Oct  4 2015 (09:49) 
+## Version: 
+## last-updated: Oct  4 2015 (10:41) 
+##           By: Thomas Alexander Gerds
+##     Update #: 26
+#----------------------------------------------------------------------
+## 
+### Commentary: 
+## 
+### Change Log:
+#----------------------------------------------------------------------
+## 
+### Code:
+##' @author Thomas A. Gerds <tag@@biostat.ku.dk>
+##' @export
+print.calibrationPlot <- function(x,...){
+    if (x$model.type=="survival"){
+        estring <- paste0("\n - a total of ",
+                          x$summary$Event[1],
+                          " were observed to have an event,\n - a total of ",
+                          x$summary$Lost,
+                          " were lost to follow-up.")
+    }else{
+         estring <- paste0("\n - a total of ",
+                           x$summary$Event[1],
+                           " were observed to have the event of interest (cause: ",x$cause,"),\n - a total of ",
+                           sum(x$summary$Event[-1]),
+                           " had a competing risk ",
+                           " \n - a total of ",
+                           x$summary$Lost,
+                           " were lost to follow-up,")
+     }
+    cat("\nCalibration of ",ifelse(x$model.type=="survival" && x$type=="survival","survival","risk")," predictions for ",
+        x$summary$n,
+        " subjects.\n\nUntil time ", x$time,
+        " a total of ",x$summary$Event.free,
+        " were observed event-free, ",
+        estring,
+        " \n - a total of ",
+        x$summary$Event.free,
+        " were observed event-free at time ",x$time,".\n",
+        sep="")
+    if (x$method=="quantile"){
+        cat("\nAverage predictions and outcome in prediction quantiles:\n\n")
+        print(x$plotFrames,...)
+    }else{
+         cat("\nSummary of predictions and outcome:\n")
+         nix <- lapply(1:length(x$plotFrames),
+                       function(n){
+                           cat("\n",names(x$plotFrames)[[n]],":\nOutcome:\n",sep="")
+                           print(summary(x$plotFrames[[1]][,1]))
+                           cat("\nPredictions:\n",sep="")                           
+                           print(summary(x$plotFrames[[1]][,2]))
+                       })
+     }
+    cat("\nOutcome frequencies (Obs) were obtained with the ",
+        ifelse(x$pseudo,
+               "jackknife pseudo-value method ",
+               ifelse(x$model.type=="survival","Kaplan-Meier method.","Aalen-Johansen method.")),
+        "\n",
+        sep="")
+}
+
+
+
+#----------------------------------------------------------------------
+### print.calibrationPlot.R ends here
